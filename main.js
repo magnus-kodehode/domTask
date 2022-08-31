9505d47593bb518c0d252848768aacfe57f3b6f1
magnus-kodehode <106583838+magnus-kodehode@users.noreply.github.com>
2022-08-31 12:50:40 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function randomColor() { //setter bakgrunnsfargen på body-elementet til en tilfeldig farge hver gang knappen trykkes på.
+    
+    document.body.style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16) 
+
+}
+
+//Dette ble veldig simpelt, men må prioritere figmaoppgaven vi fikk, da jeg er mer eller mindre 100% udugelig i det...
+
+//Trenger nok ikke vise fram dette i plenum
